9a8d7c00ce7cc81d814897b5983735079aa0b75a
mathiuxd <matzap.ai@gmail.com>
2026-02-20 21:46:08 -0500

    Improvement of even or uneven exercise

--- ProgrammingLogic/numberOddorNot/Program.cs
+++ ProgrammingLogic/numberOddorNot/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+Console.Write("Enter a number: ");
+string numberString = Console.ReadLine()!;
+
+int number = int.Parse(numberString);
+
+string message = (number % 2 == 0) ? "This number is even" : "This number isn't even";
+
+Console.WriteLine(message);
