5fceea0c5dffe3beb1b7936e4cc4c2e8d8e5cbd9
mathiuxd <matzap.ai@gmail.com>
2026-02-22 20:30:54 -0500

    feat: complete the second exercise

---
+++ ProgrammingLogic/TheGreathesNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿
+double temp = double.MinValue;
+string? numberString;
+
+for (int i = 1; i <= 3; i++)
+{
+    Console.Write("Enter a number: ");
+    numberString = Console.ReadLine();
+
+
+    if (double.TryParse(numberString, out double numberDouble))
+    {
+        if (numberDouble > temp) temp = numberDouble;
+    }
+    else
+    {
+        Console.WriteLine($"The entry: {numberString}, is incorrect.");
+        i--;
+    }
+
+}
+
+Console.WriteLine($"the largest number is {temp}");
